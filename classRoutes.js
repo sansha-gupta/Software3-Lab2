ba6c4e00d9805da5289701d31f2edfb52b182326
sansha-gupta <sansha.gupta99@gmail.com>
2020-05-05 11:00:31 +0200

    exercise 5

---
+++ classRoutes.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const express = require('express')
+const router = express.Router()
+const classList = [] // our class list array
+
+router.get('/api/list', function (req, res) {
+  res.json(classList) // Respond with JSON
+})
+router.get('/api/get/:id', function (req, res) {
+  res.json(classList[req.params.id]) // Notice the wildcard in the URL?
+  // Try browsing to /api/get/0 once you've added some entries
+})
+router.post('/api/create', function (req, res) {
+  console.log('creating the following student:', req.body.student)
+  classList.push(req.body.student)
+  res.redirect(req.baseUrl + '/api/list')
+})
+
+router.post('/api/delete', function (req, res) {
+  console.log('deleting student with ID : ', req.body.student)
+  classList.splice(req.body.student, 1)
+  res.redirect(req.baseUrl + '/api/list')
+})
+
+router.post('/api/edit', function (req, res) {
+  console.log('editing a student entry : ', req.body.studentID)
+  classList[req.body.studentID] = req.body.student
+  res.redirect(req.baseUrl + '/api/list')
+})
+
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'index.html'))
+})
+router.get('/create', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'create.html'))
+})
+router.get('/delete', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'delete.html'))
+})
+router.get('/edit', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'edit.html'))
+})
+
+module.exports = router
